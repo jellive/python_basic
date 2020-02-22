e25c24d86a37f9a38076ad0dd9bc585526ac654a
PD Jell <jellive7@gmail.com>
2020-02-22 16:22:43 +0900

    add print sep, end.

---
+++ section02-1.py
@@ -0,0 +1,29 @@
+# Section02-1
+# 파이썬 기초 코딩
+# Print 구문의 이해
+
+# 기본 출력
+print('Hello world!')
+print("Hello world!")
+print("""Hello
+world!""")
+print('''Hello
+world!''')
+
+print()
+
+
+# Seperator 옵션 사용
+
+print('T', 'E', 'S', 'T', sep='')  # sep는 join과 같다.
+print('2019', '02', '19', sep='-')  # 2019-02-19
+print('jellive7', 'google.com', sep="@")  # jellive7@google.com
+
+
+# end 옵션 사용
+
+print('Welcome to', end=' ')  # 끝을 ''로 하겠다. 즉 lnrn으로 끝내지 않겠다.
+print('the black paradise', end=' ')
+print('piano notes')
+
+print('test')
