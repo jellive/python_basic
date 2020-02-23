d0ec261e5e6098da4e1bb21a3fd6ffe7476bce46
Jellive <mystral7@naver.com>
2020-02-23 19:43:12 +0900

    add type change.

--- section04-2.py
+++ section04-2.py
@@ -43,3 +43,31 @@ print(str_o2 + str_o3)
 print(str_o1 + '3')
 print('e' in str_o4)  # include와 같음.
 print('z' not in str_o4)  # !include와 같음.
+
+# 문자열 형 변환
+print(str(77) + 'a')  # 숫자형 타입으로 하면 에러가 나지만 여기서는 안남.
+print(str(10.4))
+
+
+# 문자열 함수
+
+# a = 'jell'
+# b = 'orange'
+
+# print(a.islower())  # True
+# print(a.endswith('s'))  # False
+# print(a.capitalize())  # 첫 글자만 대문자로 바꿔 줌.
+# print(a.replace('e', 'ee'))
+# print(list(reversed(b)))  # list로 형변환을 해줘야 함. 아니면 메모리만 나옴.
+
+
+a = 'jell'
+b = 'orange'
+
+print(a[0:3])  # jel
+print(a[:4])
+print(a[0:len(a)])
+print(a[:])
+print(b[0:4:2])  # oa
+print(b[1:-2])  # ran
+print(b[::-1])  # egnaro, 즉 세 번째 파라미터는 for문의 마지막 같은 느낌으로..
