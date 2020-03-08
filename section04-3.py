3912d0daa7cad4aaa9e182e4f561d8cbc74c270a
Jellive <mystral7@naver.com>
2020-03-08 10:35:55 +0900

    add tuple.

--- section04-3.py
+++ section04-3.py
@@ -35,3 +35,31 @@ y.extend(ex)  # append도 됨. 단 append는 리스트 자체가 들어감. exte
 print(y)
 
 # 삭제: del, remove, pop
+
+
+# 튜플 (순서o, 중복o, 수정x, 삭제x)
+# const로 생각하면 될 듯?
+
+a = ()  # 튜플은 소괄호로 선언한다.
+b = (1,)
+c = (1, 2, 3, 4)
+d = (10, 100, ('a', 'b', 'c'))
+
+print(c[2])
+print(c[3])
+print(d[2][1])
+
+print(d[2:])
+print(d[2][0:2])
+
+print(c + d)  # concat과 같음.
+print(c * 3)  # tuple도 확장은 가능함.
+print()
+print()
+
+# 튜플 함수
+z = (5, 2, 1, 3, 4)
+print(z)
+print(3 in z)  # True
+print(z.index(3))
+print(z.count(1))  # 해당 파라미터의 갯수
